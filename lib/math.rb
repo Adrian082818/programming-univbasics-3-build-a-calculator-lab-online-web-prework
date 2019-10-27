bf5249fcdd043849ab631e0b1e209ab143cbff8e
Adrian Mohabir <mohabiradrian@gmail.com>
2019-10-27 16:00:00 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -9,12 +9,13 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-puts num1 * num2
+puts num2 / num1
 return 120
 end
 
 def multiplication(num1, num2)
-
+puts num1 * num2
+return 120
 end
 
 def modulo(num1, num2)
